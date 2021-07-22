741195618090c5fda3e942730a4015bf37b6a1ba
LvliguoGithub <1281358475@qq.com>
2021-07-22 15:23:34 +0800

    Create ConfigInfo.java

---
+++ src/main/java/com/llg/lqs/component/ConfigInfo.java
@@ -0,0 +1,26 @@
+package com.llg.literature.component;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "literature.conf")
+public class ConfigInfo {
+
+    private int maxSearchableCharLen;
+
+    private int maxSearchableZhCharLen;
+
+    public int getMaxSearchableCharLen() {
+        return maxSearchableCharLen;
+    }
+
+    public void setMaxSearchableCharLen(int maxSearchableCharLen) {
+        this.maxSearchableCharLen = maxSearchableCharLen;
+        this.maxSearchableZhCharLen = this.maxSearchableCharLen / 3;
+    }
+
+    public int getMaxSearchableZhCharLen() {
+        return maxSearchableZhCharLen;
+    }
+}
