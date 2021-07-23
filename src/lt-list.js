adfa12a06bece65b500924168e19e3792c89254d
LvliguoGithub <1281358475@qq.com>
2021-07-23 17:42:20 +0800

    Update lt-list.js

--- src/lt-list.js
+++ src/lt-list.js
@@ -1,52 +1,36 @@
 import React, { useState } from 'react';
-import 'antd/dist/antd.css';
-import './index.css';
-import { List, Typography, Pagination, Space, Button } from 'antd';
-import { StarOutlined, WechatOutlined } from '@ant-design/icons';
+import { Button, Input, Space } from 'antd';
+import result from "./web-api/get-search-data";
 
-function LtItem(props) {
-  const { Text, Link } = Typography;
-  return (
-  <Space direction="vertical">
-      <Link href="/lq-detail" target="_blank">{props.item.ltName}</Link>
-      <Space >
-        <Text type="secondary">发布时间</Text>
-        <Text type="secondary">发刊的期刊来源</Text>
-        <Text>DOI</Text>
-      </Space>
-      <Text>作者1，作者2</Text>
-      <Text>{props.item.desc}</Text>
-      <Space>
-        <Text>阅读数量: {0}</Text>
-        <Text>推荐数量: {0}</Text>
-        <Text>成熟度</Text>
-        <Button icon={<StarOutlined/>}>收藏</Button>
-        <Button icon={<WechatOutlined/>}>微信分享</Button>
-      </Space>
-  </Space>);
-}
+import LtList from './lt-list';
 
-function LtList(props) {
-  const [current, setCurrent] = useState(1);
+import './search-tool.css';
 
-  const paginationData = { hideOnSinglePage: true, defaultPageSize:2, total:props.data.totalElements }
-  const onChange = function(value) {
-    console.log(value)
-  }
+function SearchTool() {
+    const [data, setData] = useState({content: [], totalPages: 0});
+    const { Search } = Input;
 
+    const getSearchData = async function(value){
+        const data = await result.getSearchData(value);
+        setData(data);
+    } 
+    
     return (
-    <>
-        <List
-          pagination={ paginationData }
-          dataSource={props.data.content}
-          renderItem={item => (
-            <List.Item>
-              <LtItem item = {item}/>
-            </List.Item>
-          )}
-        />,
-    </>
-    );
+        <div className="srearch-tool">
+            <Space direction="vertical">
+                <Search placeholder="输入搜索的内容" onSearch={getSearchData} enterButton style={{ width: 600 }}/>
+                <Space>
+                    <Button type="primary">文献/出版物</Button>
+                    <Button type="primary">作者</Button>
+                    <Button type="primary">机构</Button>
+                    <Button type="primary">问题</Button>
+                    <Button type="primary">方法/成果</Button>
+                </Space>
+                <LtList data={data}/>
+            </Space>
+        </div>
+        );
 }
 
-export default LtList;
+export default SearchTool;
+
