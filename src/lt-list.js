4be3d7f7b84cfc46392d7b11916b87e1ccb95644
LvliguoGithub <1281358475@qq.com>
2021-07-23 17:41:48 +0800

    Update lt-list.js

--- src/lt-list.js
+++ src/lt-list.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 import { List, Typography, Pagination, Space, Button } from 'antd';
+import { StarOutlined, WechatOutlined } from '@ant-design/icons';
 
 function LtItem(props) {
   const { Text, Link } = Typography;
@@ -9,8 +10,8 @@ function LtItem(props) {
   <Space direction="vertical">
       <Link href="/lq-detail" target="_blank">{props.item.ltName}</Link>
       <Space >
-        <Text>发布时间</Text>
-        <Text>发刊的期刊来源</Text>
+        <Text type="secondary">发布时间</Text>
+        <Text type="secondary">发刊的期刊来源</Text>
         <Text>DOI</Text>
       </Space>
       <Text>作者1，作者2</Text>
@@ -19,8 +20,8 @@ function LtItem(props) {
         <Text>阅读数量: {0}</Text>
         <Text>推荐数量: {0}</Text>
         <Text>成熟度</Text>
-        <Button>收藏</Button>
-        <Button>微信分享</Button>
+        <Button icon={<StarOutlined/>}>收藏</Button>
+        <Button icon={<WechatOutlined/>}>微信分享</Button>
       </Space>
   </Space>);
 }
@@ -28,6 +29,7 @@ function LtItem(props) {
 function LtList(props) {
   const [current, setCurrent] = useState(1);
 
+  const paginationData = { hideOnSinglePage: true, defaultPageSize:2, total:props.data.totalElements }
   const onChange = function(value) {
     console.log(value)
   }
@@ -35,7 +37,7 @@ function LtList(props) {
     return (
     <>
         <List
-          footer={<Pagination current={current} onChange={onChange} total={props.data.totalPages} />}
+          pagination={ paginationData }
           dataSource={props.data.content}
           renderItem={item => (
             <List.Item>
